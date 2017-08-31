4294a86199832461008329e35f796409a4030e40
qichongwei <qichongwei2009@163.com>
2017-08-31 16:58:41 +0800

    add notes

--- gulptask/gulpfile.js
+++ gulptask/gulpfile.js
@@ -2,23 +2,36 @@
  * Created by qiwei on 2017/6/29.
  */
 var gulp=require('gulp'),
+    //压缩html插件  教程：http://www.ydcss.com/archives/20
     htmlmin = require('gulp-htmlmin'),
+    //合并插件，未用到。可根据情况自行添加。 教程:http://www.ydcss.com/archives/83
     concat = require('gulp-concat'),
+    //压缩css插件。教程：http://www.ydcss.com/archives/41
     cleanCSS = require('gulp-clean-css'),
+    //压缩js插件。教程：http://www.ydcss.com/archives/54
     uglify = require('gulp-uglify'),
+    //压缩图片插件。教程：http://www.ydcss.com/archives/26
     imagemin = require('gulp-imagemin'),
+    //生成文件版本号，以及替换文件。教程：https://www.npmjs.com/package/gulp-rev
     rev = require('gulp-rev'),
+    //替换文件，根据gulp-rev生成的替换文件。教程：https://www.npmjs.com/package/gulp-rev-collector
     revCollector = require('gulp-rev-collector'),
+    // 文件重命名，未用到，可根据情况自行使用。教程：https://www.npmjs.com/package/gulp-rename
     rename = require("gulp-rename"),
+    //自动添加前缀插件， 教程：http://www.ydcss.com/archives/94
     autoprefixer = require('gulp-autoprefixer'),
+    //合成雪碧图插件。 教程：https://www.npmjs.com/package/gulp.spritesmith，https://segmentfault.com/a/1190000007121606
+    // 可以比较一下与gulp-css-sprite的差别
     spritesmith=require('gulp.spritesmith'),
+    //编译less插件，教程：http://www.ydcss.com/archives/34
     less = require('gulp-less'),
+    // pump 简化了gulp流的写法，教程：https://www.npmjs.com/package/pump
     pump = require('pump');
 // css处理任务
 gulp.task('css', function () {
-   return  pump([
+    return  pump([
         gulp.src('./qiwei/styles/*.css'),
-       // css添加前缀
+        // css添加前缀
         autoprefixer({
             // browsers属性查看 can i use
             browsers: ['last 2 versions', 'Android >= 4.0'],
@@ -27,22 +40,22 @@ gulp.task('css', function () {
             //        transform: rotate(45deg);
             remove:true //是否去掉不必要的前缀 默认：true
         }),
-       // 合并css文件
-       // concat('a.css'),
-       // css压缩
+        // 合并css文件
+        // concat('a.css'),
+        // css压缩
 
         cleanCSS({keepSpecialComments: '*'}),
-       // 文件生成版本号
+        // 文件生成版本号
         rev(),
         gulp.dest('./dist/styles'),
-       rev.manifest(),
-       gulp.dest('./qiwei/json/css')
+        rev.manifest(),
+        gulp.dest('./qiwei/json/css')
     ]);
 
 });
 // js处理任务
 gulp.task('js',function () {
-   return  pump([
+    return  pump([
         gulp.src('./qiwei/scripts/all.js'),
         // 压缩js文件
         uglify(),
@@ -107,3 +120,4 @@ gulp.task('rev',['css','js'], function () {
 gulp.task('default',['rev','image'],function () {
     console.log('任务执行完成');
 });
+
